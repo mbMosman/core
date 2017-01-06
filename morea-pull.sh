1a0248c24d359ac62d09f527106f6f16d9ced92c
mbMosman <mbmosman@users.noreply.github.com>
2017-01-05 20:15:54 -0600

    Customized with info for CCIS1301

---
+++ morea-pull.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -d "./master" ]; then
+  echo "master/ directory does not exist.  Exiting..."
+  exit 1
+fi
+
+if [ ! -d "./gh-pages" ]; then
+  echo "gh-pages/ directory does not exist.  Exiting..."
+  exit 1
+fi
+
+echo "Pulling the gh-pages directory."
+( set -x ; cd ./gh-pages ; git branch -u origin/gh-pages ; git pull )
+
+echo "Pulling the master directory"
+( set -x ; cd ./master ; git pull)
---
+++ morea-vanilla-install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ $# != 2 ]; then
+    echo "morea-vanilla-install <github account> <github repo>"
+    exit 1
+fi
+
+if [ -d "./master" ]; then
+  echo "master/ directory already exists.  Exiting..."
+  exit 1
+fi
+
+if [ -d "./gh-pages" ]; then
+  echo "gh-pages/ directory already exists.  Exiting..."
+  exit 1
+fi
+
+
+echo "Creating master/ directory with repo."
+( set -x ; git clone git@github.com:$1/$2.git master)
+
+echo ""
+echo "Creating orphan branch, empty gh-pages/ directory."
+( set -x ; git clone git@github.com:$1/$2.git gh-pages)
+
+if [ -d "./gh-pages" ]; then
+  ( set -x ; cd gh-pages; git checkout --orphan gh-pages; git rm -rf .; git branch -u origin/gh-pages )
+else
+  echo "gh-pages directory not created. Exiting..."
+  exit 1
+fi
+
+echo ""
+echo "master/ and gh-pages/ directories created."
+
+echo "Adding a remote called 'core' connected to HTC HTML morea-core-custom (this can fail if already set)"
+(set x; cd ./master; git remote add core https://github.com/htc-ccis1301/morea-core-custom.git)
+
+echo "Here are the current upstream repos:"
+(set x; cd ./master; git remote -v)
+
+echo "Fetching core"
+( set -x ; cd ./master ; git fetch core)
+
+echo "Merging core into master"
+( set -x ; cd ./master ; git merge -m "merging core into master" core/master )
